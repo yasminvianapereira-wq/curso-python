200c194f29c7cf427cf7534014825a5ff72ce1a4
yasminvianapereira-wq <yasmin.viana.pereira@gmail.com>
2026-03-18 20:11:23 -0300

    Implement number increment and countdown logic
    Add a loop to print numbers with a specified increment and countdown.

---
+++ dias2.py
@@ -0,0 +1,14 @@
+inicial = int(input("digite um numero"))
+final = int(input("digite um numero"))
+incremento = int(input("digite um numero"))
+
+while inicial <= final:
+    print(f"x", (inicial)")#antes do laço adicione o incremento
+    inicial += incremento
+
+contador 10 >= 0
+
+while contador >= 0:
+    if contador == 0:
+    print("x")
+    
