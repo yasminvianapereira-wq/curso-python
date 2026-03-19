0ff2385eb501de98789d13cd47d9cfc02c61d7ce
yasminvianapereira-wq <yasmin.viana.pereira@gmail.com>
2026-03-19 19:41:58 -0300

    Implement list creation and item removal prompt
    Add functionality to create and display a list, and prompt for item removal.

---
+++ ex2-19-03-26.py
@@ -0,0 +1,10 @@
+lista = []
+for i in range (1,4):
+  item = input("Digite o item {i+ 1}")
+  lista.append(item)
+
+print(f"sua lista ficou assim: {lista}")
+
+remover = input ("digte um item pra remover.")
+for i in lisa
+print(i)
