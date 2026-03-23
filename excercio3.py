4efd0b1ac8da317a86c4296c58c36cfbffa59487
yasminvianapereira-wq <yasmin.viana.pereira@gmail.com>
2026-03-23 17:11:23 -0300

    Add input for three grades and calculate average
    Fix input prompts and calculate average correctly.

---
+++ excercio3.py
@@ -0,0 +1,6 @@
+primeira  = int(input("digite a nota um : ")
+segunda = int(input("digite a nota dois : ")
+terceira = int(input("digite a nota tres: ")
+
+print(primeira + segunda + terceira // 3)
+
