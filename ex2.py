5a890ad30a5d696c4978ceb9c2e9f7e8f8df1ad7
yasminvianapereira-wq <yasmin.viana.pereira@gmail.com>
2026-03-23 16:55:38 -0300

    Add user input and print details in ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome: ")
+idade = float(input("digite a sua idade: ")
+altura = float(input("digite a sua altura: ")
+cidade = input("digite a sua cidade: ") 
+
+print("seu nome é: " nome )
+print("sua idade é"  idade)
+print("sua altura é " altura)
+print("sua cidade é " cidade)
